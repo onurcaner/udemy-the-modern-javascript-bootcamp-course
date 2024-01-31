ee1fa2290b53518d0e4efa1450e91b0e990fd7d5
onurcaner <109483673+onurcaner@users.noreply.github.com>
2024-02-01 00:17:12 +0300

    Code backup and course checkpoint

--- shopping/source/views/account/viewAccount.ts
+++ shopping/source/views/account/viewAccount.ts
@@ -1,5 +1,5 @@
 import { UserAttributes } from '../../repositories/usersRepository';
-import { pathAdminProducts, pathAccountSignOut } from '../../routes/pagePaths';
+import { pathAccountSignOut } from '../../routes/pagePaths';
 
 export const viewUser = (user: UserAttributes): string => {
   const { isAdmin } = user;
@@ -8,11 +8,6 @@ export const viewUser = (user: UserAttributes): string => {
       <div class="columns is-centered">
         <div class="column is-one-quarter">
             <p>You are signed in as ${isAdmin ? 'an admin' : 'an user'}</p>
-            ${
-              isAdmin
-                ? `<div><a href="${pathAdminProducts}">Go to products</a><div/>`
-                : ''
-            }
             <div><a href="${pathAccountSignOut}">Sign Out</a></div>
         </div>
       </div>
